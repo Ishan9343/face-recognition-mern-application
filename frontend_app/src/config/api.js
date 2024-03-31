66c5e0597dcfb6eeb3c212b28b04f00f1ebf50f3
Ishan9343 <139035406+Ishan9343@users.noreply.github.com>
2024-03-31 13:46:43 +0530

    Update api.js

--- frontend_app/src/config/api.js
+++ frontend_app/src/config/api.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'https://face-recognition-mern-application-mjp2.vercel.app/'
+const BACKEND_URL = 'https://face-recognition-mern-application-mjp2.vercel.app'
 
 // BASE AUTH
 const AUTH_URL = '/api/auth'
